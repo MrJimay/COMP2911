9215b58303504672d2c998f5bc3b74e80572c97e
Jimmy Huang <MrJimay@users.noreply.github.com>
2016-06-16 10:07:08 +1000

    Add files via upload

---
+++ week2/src/Employee.java
@@ -0,0 +1,81 @@
+
+public class Employee {
+	private String name;
+	private int salary;
+	
+	/**
+	 * Creates a new employee
+	 * @param name
+	 * @param salary
+	 */
+	public Employee (String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	/**
+	 * Gets the name of the employee
+	 * @return Name of employee
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Gets the salary of the employee
+	 * @return Salary of employee
+	 */
+	public int getSalary() {
+		return salary;
+	}
+	
+	/**
+	 * Sets the name of the employee
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Sets the salary of the employee
+	 * @param salary
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	/**
+	 * Example: Employee JimmyClone = Jimmy.clone() creates a new Employee called JimmyClone
+	 * with the same field values as Jimmy.
+	 * @return An Employee with the same name and salary
+	 */
+	public Employee clone() {
+		Employee employee = new Employee(this.name, this.salary);
+		return employee;	
+	}
+	
+	/**
+	 * Checks whether an Employee has the same fields as this instance of the object.
+	 * @param e
+	 * @return
+	 */
+	public boolean equals(Employee e) {
+		if (e == null) return false;
+		if (this == e) return true;
+		if (this.getClass() != e.getClass()) return false;
+		Employee that = (Employee)e;
+		if (this.getName() == that.getName() && this.getSalary() == that.getSalary()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Returns the name of the Employee
+	 */
+	public String toString() {
+		return this.name;
+	}
+}
---
+++ week2/src/Manager.java
@@ -0,0 +1,59 @@
+
+public class Manager extends Employee {
+	private String HireDate;
+	
+	/**
+	 * Creates a new manager
+	 * @param name
+	 * @param salary
+	 * @param HireDate
+	 */
+	public Manager(String name, int salary, String HireDate) {
+		super(name, salary);
+		this.HireDate = HireDate;
+		
+	}
+	
+	/**
+	 * Sets the HireDate of the manager
+	 * @param HireDate
+	 */
+	public void setHireDate(String HireDate) {
+		this.HireDate = HireDate;
+	}
+	
+	/**
+	 * Gets the HireDate of the manager
+	 * @return HireDate
+	 */
+	public String getHireDate() {
+		return this.HireDate;
+	}
+	
+	/**
+	 * Example: Manager JimmyClone = Jimmy.clone() creates a new Manager called JimmyClone
+	 * with the same field values as Jimmy.
+	 * @return An Manager with the same name, salary and hire date
+	 */
+	public Manager clone() {
+		Manager manager = new Manager(this.getName(), this.getSalary(), this.HireDate);
+		return manager;	
+	}
+	
+	/**
+	 * Checks whether a Manager has the same fields as this instance of the object.
+	 * @param e
+	 * @return
+	 */
+	public boolean equals(Manager m) {
+		if (m == null) return false;
+		if (this == m) return true;
+		if (this.getClass() != m.getClass()) return false;
+		Manager that = (Manager)m;
+		if (this.getName() == that.getName() && this.getSalary() == that.getSalary()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
