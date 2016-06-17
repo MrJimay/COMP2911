b4b162693db99f0118eb8b4c985107a59552938a
Jimmy <mrjimay@gmail.com>
2016-06-17 16:08:31 +1000

    Generic set implementation

---
+++ week5/src/ConcreteSet.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ConcreteSet<E> implements Set<E> {
+	ArrayList<E> elements;
+	
+	public ConcreteSet() {
+		this.elements = new ArrayList<E>();
+	}
+	
+	@Override
+	public boolean add(E element) {
+		if (!elements.contains(element)) {
+			elements.add(element);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void remove(E element) {
+		elements.remove(element);
+	}
+	
+	@Override
+	public boolean contains(E element) {
+		return elements.contains(element);
+	}
+	
+	@Override
+	public Set<E> union(Set<E> set) {
+		Set<E> unionSet = set;
+		for (E element : elements) {
+			unionSet.add(element);
+		}
+		return unionSet;
+	}
+
+	@Override
+	public Set<E> intersection(Set<E> set) {
+		Set<E> intersectionSet = new ConcreteSet<E>();
+		for (E element : elements) {
+			if (set.contains(element)) {
+				intersectionSet.add(element);
+			}
+		}
+		return intersectionSet;
+	}
+
+	@Override
+	public boolean isSubset(Set<E> set) {
+		for (E element : elements) {
+			if (!set.contains(element)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || o.getClass()!=this.getClass()) return false;
+		Set<E> that = (Set<E>) o;
+		if (this.elements.equals(that)) return true;
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return elements.iterator();
+	}
+}
---
+++ week5/src/Set.java
@@ -0,0 +1,15 @@
+
+public interface Set<E> extends Iterable<E> {
+	boolean add(E element);
+	void remove(E element);
+	boolean contains(E element);
+	Set<E> union(Set<E> set);
+	Set<E> intersection(Set<E> set);
+	/**
+	 * Checks if this set is a subset of the set that is passed in
+	 * @param set
+	 * @return
+	 */
+	boolean isSubset(Set<E> set);
+	boolean equals(Object o);
+}
